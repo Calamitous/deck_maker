2fe1f104967f43a4112dd75a582aa632761b65eb
Eric Budd <eric@readmedia.com>
2014-12-21 00:23:02 -0600

    Added nicer inspects and player counts

--- deck.rb
+++ deck.rb
@@ -9,15 +9,26 @@ class Deck < Array
     raise "Not enough cards to deal #{count} to each player." unless @deck.length >= (hands.length * count)
     count.times { hands.each{|hand| hand.draw } }
   end
+
+  def inspect
+    return_value = ""
+    self.deck.each_with_index do |card, index|
+      return_value << "* #{index}: #{card}\n"
+    end
+    return_value
+  end
 end
 
 class Hand < Array
   attr_accessor :hand
   attr_reader :deck
+  @@player_count = 1
 
-  def initialize(deck)
+  def initialize(deck, name = "Player #{@@player_count}")
     @deck = deck.deck
     @hand = []
+    @name = name
+    @@player_count += 1
   end
 
   def draw
@@ -29,9 +40,16 @@ class Hand < Array
     raise 'You can\'t play or discard a card you don\'t have' unless @hand[card]
     @hand.slice!(card)
   end
+
+  def inspect
+    return_value = "#{@name}\n"
+    self.hand.each_with_index do |card, index|
+      return_value << "  - #{index}: #{card}\n"
+    end
+    return_value
+  end
 end
 
-# Initial hand should be 4 cards
 @initial_deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 @deck = Deck.new(@initial_deck)
