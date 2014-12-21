07dc0d5bbd11c3c3e1f0e8c8c0aa08f5863146e8
Eric Budd <eric@readmedia.com>
2014-12-20 20:09:43 -0600

    Added Hand class

--- deck.rb
+++ deck.rb
@@ -1,23 +1,41 @@
 class Deck < Array
+  attr_reader :deck
+
+  def initialize(initial)
+    @deck = initial.shuffle
+  end
+
+  def deal(count, hands)
+    raise "Not enough cards to deal #{count} to each player." unless @deck.length >= (hands.length * count)
+    count.times do
+      hands.each{|hand| hand.draw }
+    end
+
+  end
+end
+
+class Hand < Array
   attr_accessor :hand
   attr_reader :deck
 
-  def initialize(initial, hand_size)
-    @deck = INITAL_DECK.shuffle
+  def initialize(deck)
+    @deck = deck
     @hand = []
-
-    hand_size.times { self.draw }
   end
 
   def draw
-    raise "Ooops" unless @deck.length > 0
+    raise "Deck is empty" unless @deck.length > 0
     @hand << @deck.pop
   end
 
 end
 
-INITAL_DECK = [1, 2, 3, 4, 5, 6, 7, 8]
-
 # Initial hand should be 4 cards
-deck = Deck.new(INITAL_DECK, 4)
+initial_deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+deck = Deck.new(initial_deck)
+p1 = Hand.new(deck)
+p2 = Hand.new(deck)
+
+deck.deal(4, [p1, p2])
 
